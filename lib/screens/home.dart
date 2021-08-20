d4de329537784329da6fc37e7561ee0d08e2f9b6
himmatsingh019 <himmatsingh19k@gmail.com>
2021-08-20 12:38:41 +0530

    User can delete and mark complete a task

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lazy_do/constants/theme.dart';
-import 'package:lazy_do/constants/widgets.dart';
 import 'package:lazy_do/model/task_model.dart';
 import 'package:lazy_do/screens/login.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -24,6 +23,8 @@ class _HomeScreenState extends State<HomeScreen> {
   TextEditingController titleController = TextEditingController();
   final auth = FirebaseAuth.instance;
   late Box<TaskModel> taskModelBox;
+  bool _isFav = false;
+  bool _isComplete = false;
 
   @override
   void initState() {
@@ -41,78 +42,80 @@ class _HomeScreenState extends State<HomeScreen> {
           backgroundColor: ColorData.white_color),
       drawer: Drawer(
         elevation: 0,
-        child: Container(
-          padding: EdgeInsets.only(left: 2),
-          color: ColorData.white_color,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0, left: 8),
-                child: Row(
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundColor: Colors.blue,
-                      radius: 36,
-                    ),
-                    SizedBox(width: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          widget.name.toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            color: Colors.black,
+        child: SafeArea(
+          child: Container(
+            padding: EdgeInsets.only(left: 2),
+            color: ColorData.white_color,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0, left: 8),
+                  child: Row(
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.blue,
+                        radius: 36,
+                      ),
+                      SizedBox(width: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            widget.name.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              color: Colors.black,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 6),
-                        Text(
-                          widget.email.toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            color: Colors.black,
+                          SizedBox(height: 6),
+                          Text(
+                            widget.email.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              color: Colors.black,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 16),
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Settings',
-                  style: TextStyle(
-                    fontSize: 18,
-                    letterSpacing: 1.4,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.black,
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => LoginScreen(),
+                SizedBox(height: 16),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Settings',
+                    style: TextStyle(
+                      fontSize: 18,
+                      letterSpacing: 1.4,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.black,
                     ),
-                  );
-                },
-                child: Text(
-                  'SignOut',
-                  style: TextStyle(
-                    fontSize: 18,
-                    letterSpacing: 1.4,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.black,
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => LoginScreen(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'SignOut',
+                    style: TextStyle(
+                      fontSize: 18,
+                      letterSpacing: 1.4,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -141,12 +144,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
                           taskModelBox.add(taskModel);
                           Get.back();
-                          Get.snackbar(
-                            'Task created',
-                            '',
-                            snackPosition: SnackPosition.BOTTOM,
-                            duration: Duration(seconds: 1),
-                          );
+                          showSnackBar('Task created', '');
                         }
                       },
                       child: Text('Create'),
@@ -173,9 +171,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
           GestureDetector(
-            onTap: () {
-              // databaseHelper.convert();
-            },
+            onTap: () {},
             child: ListTile(
               minLeadingWidth: 0,
               leading: Icon(Icons.star_border_outlined),
@@ -206,8 +202,86 @@ class _HomeScreenState extends State<HomeScreen> {
                   itemBuilder: (context, index) {
                     final int key = keys[index];
                     final TaskModel? tasks = todos.get(key);
-                    return TaskCard(
-                      title: tasks!.title,
+                    return Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Container(
+                        height: 100,
+                        padding: EdgeInsets.only(left: 12, top: 8),
+                        width: Get.width,
+                        decoration: BoxDecoration(
+                          color:
+                              _isComplete ? Colors.green[200] : Colors.white60,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.4),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tasks!.title,
+                              maxLines: 2,
+                              style: const TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.w700),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    if (_isComplete == false) {
+                                      setState(() {
+                                        _isComplete = true;
+                                      });
+
+                                      showSnackBar('Completed',
+                                          'The task has been marked as complete');
+                                    } else {
+                                      setState(() {
+                                        _isComplete = false;
+                                      });
+                                    }
+                                  },
+                                  icon: Icon(Icons.done),
+                                ),
+                                IconButton(
+                                  splashRadius: 1,
+                                  onPressed: () {
+                                    if (_isFav == false) {
+                                      setState(() {
+                                        _isFav = true;
+                                      });
+                                    } else {
+                                      setState(() {
+                                        _isFav = false;
+                                      });
+                                    }
+                                  },
+                                  icon: Icon(Icons.star),
+                                  color: _isFav
+                                      ? Colors.yellow[700]
+                                      : Colors.black,
+                                ),
+                                IconButton(
+                                  splashRadius: 1,
+                                  onPressed: () {
+                                    todos.delete(key);
+                                    showSnackBar('Task deleted', '');
+                                  },
+                                  icon: Icon(Icons.delete_outline_outlined),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
                     );
                   },
                   separatorBuilder: (context, index) => Divider(),
@@ -219,4 +293,13 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+}
+
+showSnackBar(String title, message) {
+  return Get.snackbar(
+    title,
+    message,
+    duration: Duration(seconds: 2),
+    snackPosition: SnackPosition.BOTTOM,
+  );
 }
